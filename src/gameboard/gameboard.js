f0bbda14b9d44108585a6ef42f204aa98b727ee8
MiguelDeTrool <migueldetrool@gmail.com>
2022-10-28 23:42:12 +0200

    Implement indicated gameboard functionality

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -24,25 +24,57 @@ class Gameboard {
 
   placeShip(origin, length, orientation) {
     this.#ships.push(new Ship(length));
+    // orientation 0 for horizontal, 1 for vertical
     this.#fillSquares(origin, length, orientation, this.#currentShipID);
     this.#currentShipID++;
   }
 
-  hasShip(coor) {
-    return this.#grid[coor[0]][coor[1]].hasShip;
+  squareInfo(coor) {
+    return this.#grid[coor[0]][coor[1]];
   }
 
   #fillSquares(origin, length, orientation, identifier) {
-    // orientation 0 for horizontal, 1 for vertical
+    let x = origin[0];
+    let y = origin[1];
     if (orientation === 0) {
       for (let i = origin[0]; i < origin[0] + length; i++) {
-        this.#grid[i][origin[1]].hasShip = true;
+        this.#grid[i][y].hasShip = true;
+        this.#grid[i][y].shipID = identifier;
       }
     } else {
       for (let i = origin[1]; i < origin[1] + length; i++) {
-        this.#grid[origin[0]][i].hasShip = true;
+        this.#grid[x][i].hasShip = true;
+        this.#grid[x][i].shipID = identifier;
       }
     }
+  }
+
+  isShipSunk(identifier) {
+    return this.#ships[identifier].isSunk();
+  }
+
+  receiveAttack(coor) {
+    let x = coor[0];
+    let y = coor[1];
+
+    if (this.#grid[x][y].hit === false) {
+      this.#grid[x][y].hit = true;
+    } else {
+      throw new Error("Already hit");
+    }
+
+    if (this.#grid[x][y].hasShip === true) {
+      let ID = this.#grid[x][y].shipID;
+      this.#ships[ID].hit();
+    }
+  }
+
+  areAllShipsSunk() {
+    const result = this.#ships.reduce(
+      (prev, currShip) => prev && currShip.isSunk(),
+      true
+    );
+    return result;
   }
 }
 
--- src/gameboard/gameboard.test.js
+++ src/gameboard/gameboard.test.js
@@ -5,6 +5,61 @@ test("It creates a grid and can know if there is a ship on a square", () => {
 
   board.placeShip([3, 3], 2, 0);
 
-  expect(board.hasShip([4, 5])).toBe(false);
-  expect(board.hasShip([3, 3])).toBe(true);
+  expect(board.squareInfo([3, 3])).toMatchObject({ hasShip: true });
+  expect(board.squareInfo([4, 5])).toMatchObject({ hasShip: false });
+});
+
+test("It knows which ship is on a given square", () => {
+  let board = new Gameboard(8);
+
+  board.placeShip([1, 1], 2, 0);
+  board.placeShip([3, 3], 2, 0);
+
+  expect(board.squareInfo([0, 0])).toMatchObject({ hasShip: false });
+  expect(board.squareInfo([1, 1])).toMatchObject({ hasShip: true, shipID: 0 });
+  expect(board.squareInfo([3, 3])).toMatchObject({ hasShip: true, shipID: 1 });
+});
+
+test("It keeps tracks of hits, on ships and in sea", () => {
+  let board = new Gameboard(8);
+
+  board.placeShip([3, 3], 2, 0);
+  board.receiveAttack([3, 3]);
+  board.receiveAttack([4, 3]);
+
+  board.receiveAttack([4, 4]);
+
+  expect(board.squareInfo([3, 3])).toMatchObject({ hasShip: true, hit: true });
+  expect(board.isShipSunk(0)).toBe(true);
+
+  expect(board.squareInfo([4, 4])).toMatchObject({ hasShip: false, hit: true });
+});
+
+test("It reports if the square being attacked has already been hit", () => {
+  let board = new Gameboard(8);
+
+  board.placeShip([3, 3], 2, 0);
+  board.receiveAttack([3, 3]);
+
+  expect(() => board.receiveAttack([3, 3])).toThrow("Already hit");
+
+  board.receiveAttack([4, 4]);
+  expect(() => board.receiveAttack([4, 4])).toThrow("Already hit");
+});
+
+test("It reports if all ships are sunk", () => {
+  let board = new Gameboard(8);
+
+  board.placeShip([3, 3], 2, 0);
+  board.placeShip([5, 5], 2, 1);
+
+  board.receiveAttack([3, 3]);
+  board.receiveAttack([4, 3]);
+
+  expect(board.areAllShipsSunk()).toBe(false);
+
+  board.receiveAttack([5, 5]);
+  board.receiveAttack([5, 6]);
+
+  expect(board.areAllShipsSunk()).toBe(true);
 });
